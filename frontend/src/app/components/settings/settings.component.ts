8d92d8175aa9c0f636abf17720155ed8e5ccd84e
AtilayBasaran <abasaran77@gmail.com>
2022-04-29 16:08:57 +0300

    Settings Update
    settings page update

--- frontend/src/app/components/settings/settings.component.ts
+++ frontend/src/app/components/settings/settings.component.ts
@@ -1,15 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatSort, Sort} from '@angular/material/sort';
+import {LiveAnnouncer} from '@angular/cdk/a11y';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+
+export interface StudentTable {
+  name: string;
+  position: number;
+  mail: string;
+  
+}
+
+const ELEMENT_DATA: StudentTable[] = [
+  {position: 1, name: 'salihh', mail: "salih@gmail.com"},
+  {position: 2, name: 'salih', mail: "salih@gmail.com"},
+  {position: 3, name: 'salih', mail: "salih@gmail.com"},
+  {position: 4, name: 'salih', mail: "salih@gmail.com"},
+  {position: 5, name: 'salih', mail: "salih@gmail.com"},
+  {position: 6, name: 'salih', mail: "salih@gmail.com"},
+  {position: 7, name: 'atılay', mail: "salih@gmail.com"},
+  {position: 8, name: 'salih', mail: "salih@gmail.com"},
+  {position: 9, name: 'salih', mail: "salih@gmail.com"},
+  {position: 10, name: 'salih', mail: "salih@gmail.com"},
+  {position: 11, name: 'salih', mail: "salih@gmail.com"},
+  {position: 12, name: 'salih', mail: "salih@gmail.com"},
+];
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
-export class SettingsComponent implements OnInit {
+export class SettingsComponent  implements AfterViewInit {
+  columnsToDisplay : string[] = ['position', 'name', 'mail'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  expandedElement: StudentTable | null;
+  
 
-  constructor() { }
+  constructor(private _liveAnnouncer: LiveAnnouncer) {}//buraya daha sonra database gelecek
 
-  ngOnInit(): void {
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+  announceSortChange(sortState: Sort) {
+    if (sortState.direction) {
+      this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
+    } else {
+      this._liveAnnouncer.announce('Sorting cleared');
+    }
   }
 
 }
+
